1e6a01192aa684cf555bca3ca0909a4f2f072e36
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-06-13 21:31:24 +0800

    master

--- runtime/X86Lib2/MathExtension.h
+++
@@ -1,4 +0,0 @@
-#include <math.h>
-inline double sin(int x){return sin((double)x);}
-inline double cos(int x){return cos((double)x);}
-inline double sqrt(int x){return sqrt((double)x);} 
--- runtime/X86Lib2_old/lock_free_barrier.h
+++
@@ -1,4 +0,0 @@
-extern volatile char *barrierBuffer;
-void masterSync(int n);
-void workerSync(const int);
-void allocBarrier(int n);
